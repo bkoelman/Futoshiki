6d0281e60198475fca21f773aa2cb2db3c3abf7e
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2018-08-28 11:05:56 +0200

    Added keyboard shortcuts

--- src/app/digitbar/digitbar.component.ts
+++ src/app/digitbar/digitbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-digitbar',
+  host: {'(window:keydown)': 'onKeyDown($event)'},
   templateUrl: './digitbar.component.html'
 })
 export class DigitbarComponent implements OnInit {
@@ -15,5 +16,15 @@ export class DigitbarComponent implements OnInit {
     let button = <HTMLElement>event.target;
     let value = button.innerText == "X" ? undefined : parseInt(button.innerText);
     this.digitClicked.emit(value);
+  }
+
+  onKeyDown(event: KeyboardEvent) {
+    if (event.key == '1' || event.key == '2' || event.key == '3' || event.key == '4') {
+      let value = parseInt(event.key);
+      this.digitClicked.emit(value);
+    }
+    else if (event.code == 'Delete' || event.code == 'Backspace') {
+      this.digitClicked.emit(undefined);
+    }
   }
 }
