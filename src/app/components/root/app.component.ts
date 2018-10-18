2458fb177573ab60fa22977068f1e6a93e75df84
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2018-10-18 18:04:30 +0200

    added visitor tracking

--- src/app/components/root/app.component.ts
+++ src/app/components/root/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  isTrackerEnabled = environment.production;
+}
