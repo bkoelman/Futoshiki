33fce107e65da989b09f6e81ed27f60d339f2499
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2018-10-18 13:18:19 +0200

    alternative build version management

--- src/write-build-version.js
+++ src/write-build-version.js
@@ -4,6 +4,6 @@ const date = new Date();
 const timestamp = date.getSeconds() + 60 * date.getMinutes() + 60 * 60 * date.getHours();
 const version = `${date.getFullYear()}.${date.getMonth()}.${date.getDate()}.${timestamp}`;
 
-const path = __dirname + '/app/build-version.ts';
+const path = __dirname + '/app/build-version.prod.ts';
 console.log(`Writing build version '${version}' to ${path}`);
 fs.writeFileSync(path, `export const buildVersion = '${version}';\n`);
