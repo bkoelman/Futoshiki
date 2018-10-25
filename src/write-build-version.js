1c310257b9f783822c6e9baf9b9af9ef165361c6
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2018-10-25 10:15:09 +0200

    fixed one-off month in version

--- src/write-build-version.js
+++ src/write-build-version.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 const date = new Date();
 const timestamp = date.getSeconds() + 60 * date.getMinutes() + 60 * 60 * date.getHours();
-const version = `${date.getFullYear()}.${date.getMonth()}.${date.getDate()}.${timestamp}`;
+const version = `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}.${timestamp}`;
 
 const path = __dirname + '/app/build-version.prod.ts';
 console.log(`Writing build version '${version}' to ${path}`);
